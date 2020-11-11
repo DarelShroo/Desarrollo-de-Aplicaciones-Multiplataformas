21cc601403c6a93037be4666202b30c2ef5b7db2
Darel M <cls18drl@gmail.com>
2020-11-11 00:34:25 +0000

    Añadido guia -> SQL.pdf

--- Programación/Java/CountWords.java
+++ Programación/Java/CountWords.java
@@ -3,25 +3,21 @@ import java.util.Scanner;
 public class CountWords {
 
 	public static void main(String[] args) {
-
+		
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Escriba una frase");
-		String text = input.nextLine().trim();
+		System.out.println("Escribe una frase");
+		String frase = input.nextLine();
 		input.close();
 		
 		int countWords = 1;
 		
-		for (int i=0; i<text.length(); i++) {
-			
-			if (Character.isLetter(text.charAt(i)) == false && Character.isLetter(text.charAt(i+1))) {
+		for (int i=0; i<frase.length(); i++) {
+			if((Character.isLetter(frase.charAt(i))==false) && (Character.isLetter(frase.charAt(i+1)))) {
 				countWords++;
 			}
-			
 		}
-		
-		System.out.println("Hay " + countWords + " palabras");
-
+		System.out.println("Esta frase tiene en total " + countWords + " palabras." );
 	}
 
 }
